cc624e68492ea285817766737d97f9891421a5bd
Adrian <hannsadrian@gmail.com>
2020-04-28 23:31:02 +0200

    Add locaiton information

---
+++ Wikipeter/location/LocationManager.swift
@@ -0,0 +1,62 @@
+//
+//  LocationManager.swift
+//  Wikipeter
+//
+//  Created by Adrian Böhme on 28.04.20.
+//  Copyright © 2020 Adrian Böhme. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, ObservableObject {
+    private let geocoder = CLGeocoder()
+    private let locationManager = CLLocationManager()
+    let objectWillChange = PassthroughSubject<Void, Never>()
+
+    @Published var location: CLLocation?
+
+    override init() {
+        super.init()
+
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+
+    @Published var placemark: CLPlacemark? {
+        willSet { objectWillChange.send() }
+    }
+
+    private func geocode() {
+        guard let location = self.location else { return }
+        geocoder.reverseGeocodeLocation(location, completionHandler: { (places, error) in
+            if error == nil {
+                self.placemark = places?[0]
+            } else {
+                self.placemark = nil
+            }
+        })
+    }
+}
+
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.location = location
+        self.geocode()
+    }
+}
+
+extension CLLocation {
+    var latitude: Double {
+        return self.coordinate.latitude
+    }
+
+    var longitude: Double {
+        return self.coordinate.longitude
+    }
+}
